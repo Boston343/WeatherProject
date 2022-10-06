ed947962fecd069d63937dce7c983aecdd7b8030
Boston343 <branden.austin1@gmail.com>
2022-10-05 22:39:09 -0400

    user can now input a city and the weather from that city will be displayed. This also catches an invalid city name which gets a response of '404' and tells the user it is an invalid city.

--- app.js
+++ app.js
@@ -30,22 +30,46 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
     console.log("Server is up and running.");
 
-    const weatherRequest = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=Miami&appid=" + weatherApiKey;
+    var fileName = "index.html";
+    res.sendFile(path.join(__dirname, "/" + fileName));
+    console.log('Sent:', __dirname + "/" + fileName);
+    
+});
+
+// -------------------------------------------------------------
+// -------------------- Post Requests --------------------------
+// -------------------------------------------------------------
+app.post('/', (req, res) => {
+    var query = req.body.cityName;
+    const weatherRequest = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + query + "&appid=" + weatherApiKey;
     https.get(weatherRequest, (weatherResponse) => {
         console.log('statusCode:', weatherResponse.statusCode);
         console.log('statusMessage:', weatherResponse.statusMessage);
         console.log('headers:', weatherResponse.headers);
 
-        weatherResponse.on('data', (d) => {
-            var weatherData = JSON.parse(d);
-            console.log(weatherData);
-            var temp = weatherData.main.temp;
-            console.log("temperature: " + temp + "F");
-            var desc = weatherData.weather[0].description;
-            console.log("description: " + desc);
-        });
+        if ( weatherResponse.statusCode === 404 ) {
+            res.send("<h1><u>" + req.body.cityName + "</u> is an invalid city. Please try again.</h1>");
+        }
+        else {
+            weatherResponse.on('data', (d) => {
+                var weatherData = JSON.parse(d);
+                console.log(weatherData);
+                var temp = weatherData.main.temp;
+                console.log("temperature: " + temp + "F");
+                var desc = weatherData.weather[0].description;
+                console.log("description: " + desc);
+                var iconURL = "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png";
+    
+                // send data to browser. Only one "res.send()" per get function
+                res.write("<h1>The weather in " + query + " is currently " + desc + ".</h1>")
+                res.write("<h1>The temperature in " + query + " is currently " + temp + " degrees farenheit.</h1>");
+                res.write("<img src=" + iconURL + " alt=\"weather picture\"></img>")
+                res.send();
+            });
+        }
 
     }).on('error', (e) => {
         console.error(e);
     });
 });
+
